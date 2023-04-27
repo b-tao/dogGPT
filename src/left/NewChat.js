a0612c0e0090d0bb81af8d513079550ac8fc39a9
Brian <briantao@bu.edu>
2023-04-27 19:43:11 -0400

    made mobile responsive

--- src/left/NewChat.js
+++ src/left/NewChat.js
@@ -4,15 +4,19 @@ import dog from '../right/dog.jpg';
 export default function NewChat() {
     return (
         <div className='overall'>
-            <div className='newChatSection'>
-                <p className="NewChat"> + &ensp;&ensp;New Chat</p></div>
+            <div className='NewChatSection'>
+                <div className='NewChatButton'>
+                <p className="NewChat"> + </p>
+                <p className='text-container'>New Chat</p></div></div>
             <div className='current chats'></div>
             <div className='extra_icons'>
                 <div className="Contact_wrapper">
                     <div className="imageContainer">
                         <img className="ImageID" src={dog} alt="dog icon" />
-                    </div> 
-                    <p>&ensp;Contact Us&ensp;</p>
+                    </div>
+                    <div className='text-container'>            
+                            <p className='extra_text'>&ensp;Contact Us&ensp;</p>
+                    </div>
                 </div>
             </div>
 
